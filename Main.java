0a133a4fac31faa622005622f57de809b45a0c5e
TinaD1906 <dotina87@gmail.com>
2024-12-13 18:15:26 +0000

    yo moma

--- Main.java
+++ Main.java
@@ -5,6 +5,19 @@ public class Main
   public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
+    Oven v = new Oven();
+    System.out.println("Maximum oven temperature");
+    int maxTemp = sc.nextInt();
+
+    System.out.println("Starting temperature of the oven");
+    int currentTemp = sc.nextInt(); 
+    
+     
+
+
+
+
+    Scanner sc = new Scanner(System.in);
     Person p = new Person();
     System.out.println("Enter the person's first name:");
     String firstName = sc.nextLine();
--- Oven.java
+++ Oven.java
@@ -1,4 +1,57 @@
 public class Oven
 {
+    private int maxTemp;
+    private int currentTemp;
 
+    public void setMaxTemp(int input)
+    {
+        maxTemp = input;
+    }
+
+    public void setcurrentTemp(int input)
+    {
+        currentTemp = input;
+    }
+
+    public int getmaxTemp()
+    {
+        return maxTemp;
+    }
+
+    public int getcurrentTemp()
+    {
+        return currentTemp;
+    }
+
+    public void turnOff()
+    {
+        currentTemp = 0;
+    }
+
+    public boolean isOn()
+    {
+        if (currentTemp > 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    public void preheat (int temp)
+    {
+       
+        if (temp > 0)
+        {
+            temp = currentTemp;
+            if (temp > maxTemp)
+            {
+                currentTemp = maxTemp;
+            }
+        }
+    public String toString()
+    {
+        System.out.println("New oven with a maximum temperature of" + maxTemp + " and a starting temperature of" + currentTemp+ "degrees");
+    }
 }
